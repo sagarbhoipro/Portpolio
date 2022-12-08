865a9189fe4f9e26a2c354cd4a5c1bb53344ea82
sagarbhoipro <118772412+sagarbhoipro@users.noreply.github.com>
2022-12-08 22:41:59 +0530

    Add files via upload

---
+++ ww.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		
+	</title>
+</head>
+<body>
+	<h1
+	id=my></h1>
+
+</body>
+<script type="text/javascript">
+	var x= 10
+	var y =11
+	alert{
+		y
+	}	
+</script>
+</html>
